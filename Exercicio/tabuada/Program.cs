fa6291fbcddfec3c3830cd6cd33e7f1af8b61933
MatheusEstevess <matheusesantos@gmail.com>
2019-09-20 11:14:35 -0300

    add a tabuada

---
+++ Exercicio/tabuada/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tabuada do 5");
+            for (int x = 5; x <=50; x+=5){
+                Console.WriteLine(x + " ");
+            }
+        }
+    }
+}
