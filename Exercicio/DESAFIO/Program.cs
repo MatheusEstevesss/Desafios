bed8dc89f8065abbe921d0546282fd93fea2dc2a
MatheusEstevess <matheusesantos@gmail.com>
2019-09-25 10:23:40 -0300

    att 25/09/2019

---
+++ Exercicio/DESAFIO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DESAFIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] vetor = {1,2,3,4,5};
+        }
+    }
+}
---
+++ Exercicio/sz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace sz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, c, l, y;
+       Console.WriteLine("Digite a quantidade de triangulos: ");
+       y = int.Parse(Console.ReadLine());
+      Console.WriteLine("Digite o Tamanho: ");
+       n = int.Parse(Console.ReadLine());
+        
+
+       for (l=1;l<=n;l++) {
+        for(c=1;c<=l;c++)
+         Console.Write("*");
+       Console.Write("\n");
+       }
+       if(y>1){
+           for (l=1;l<=n;l++) {
+            for(c=1;c<=l;c++)
+            Console.Write("*");
+            Console.Write("\n");
+       }
+       }
+        }
+    }
+}
