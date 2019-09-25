9319f2c4dbbb6245ce07a8dd0d8eb2d98d08e9d2
MatheusEstevess <matheusesantos@gmail.com>
2019-09-25 11:28:32 -0300

    att 25/09/2019

---
+++ Exercicios2(Matrizes)/Parimpar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Parimpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Matriz");
+            
+            int [] vetor= new int[6];
+            int [] dados = {1,2,3,4,5,6};
+                for(int i=0; i < 6; i++){
+                    dados[i]= i;
+                if (dados%2){
+                    Console.WriteLine("seu núemro é par:", dados);
+                } else {
+                    Console.WriteLine("seu núemro é par:", dados);
+                }
+        }
+    }
+}
+}
