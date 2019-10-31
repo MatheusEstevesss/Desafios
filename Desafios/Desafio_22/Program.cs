68fcabf2c297a10d81f9e1954bba5e235144472d
MatheusEstevess <matheusesantos@gmail.com>
2019-10-31 10:59:42 -0300

    desafios denovo

---
+++ Desafios/Desafio_22/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Desafio_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("+++++   Soma das Matrizes   +++++");
+
+            int [,] num = new int [3 , 3];
+            Random random = new Random();
+
+            for (int x = 0; x < 3; x++){
+                for (int y = 0; y < 3; y++){
+                    num [x , y] = random.Next(1, 100);
+                    System.Console.WriteLine($"Sua Matriz é: {num [x , y]}");
+                }
+            }
+
+            int soma = num [0,0] + num [0,1] + num [1,0] + num [0,2] + num [2,0] + num [1,1] + num [1,2] + num [2,2];
+
+            
+            System.Console.WriteLine($"A soma dos números da matriz é: {soma}");
+        }
+    }
+}
---
+++ Desafios/Desafio_23/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Desafio_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+
+            int maior = 0;
+            int menor = 0;
+            int posicaoMaior = 0;
+            int posicaoMenor = 0;
+
+            System.Console.WriteLine("++++++++++   Posição da Matriz   ++++++++++");
+            
+            int [] vetor = new int [10];
+            Random random = new Random();
+
+            maior = vetor [0];
+            menor = vetor [0];
+
+            for (int x = 0; x < 10; x++){
+                vetor [x] = random.Next(1, 100);
+
+                foreach (int num in vetor){
+                    if (num > maior){
+                        maior = num;
+                        posicaoMaior = x + 1;
+                    } else if (num < menor){
+                        menor = num;
+                        posicaoMenor = x + 1;
+                    }
+                }
+                System.Console.WriteLine(vetor [x]);
+            }
+
+            System.Console.WriteLine($"O maior número da matriz é: {maior} e está na posição: {posicaoMaior}");
+            System.Console.WriteLine($"O menor número da matriz é: {menor} e está na posição: {posicaoMenor}");
+        }
+    }
+}
